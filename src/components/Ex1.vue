5b3336f7fa680c3ac7b0746f88fcdd4ea84c71c4
hazellng <hzlnguyen03@gmail.com>
2026-09-14 08:37:04 +0800

    ex1,2,3

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -3,22 +3,26 @@
 import { ref } from 'vue';
 
 const id = ref('circle')
+//const cls = ref('ciccle')
 
 
 function mOver() {
     // add code here
+    id.value = "square"
     
 }
 
 function mOut() {
     // add code here
+    id.value = "circle"
    
 }
 </script>
 
 <template>
     <!-- modify code below -->
-    <div v-bind:id="id">
+    <div :id="id" @mouseover="mOver" @mouseout="mOut">
+    <!-- <div :id="shape" :class="cls" @mouseover="mOver" @mouseout="mOut">    -->
         Mouse Over Me
     </div>
 
@@ -41,4 +45,20 @@ function mOut() {
     text-align: center;
     padding-top: 60px;
 }
+
+/* #shape {
+    width: 150px;
+    height: 150px;
+    background-color: goldenrod;
+    text-align: center;
+    padding-top: 60px;
+}
+
+.circle {
+    border-radius: 50%
+}
+
+.square {
+    border-radius: 0%
+} */
 </style>
